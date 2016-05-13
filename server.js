928b4a07367ed89e4a33a03da91e963c482c83e9
Krasimir Todorov <krasytod@gmail.com>
2016-05-13 12:05:06 +0000

    final

--- server.js
+++ server.js
@@ -27,6 +27,20 @@ app.use(passport.session());
 
 routes(app, passport);
 
+
+app.get('*', function(req, res){
+	var ip = "ipaddress: "+req.headers['x-forwarded-for']; //req.connection.remoteAddress)
+	//bg,en-US;q=0.8,en;q=0.6
+	var lang ="language: "+ req.headers["accept-language"].split(",")[0];
+	var client_info  = req.headers['user-agent'] ; 
+	var os_match = /(\x28.*?\x29)/g 
+	client_info = "software: "+os_match.exec(client_info)[1]
+	//res.write(client_info)
+	res.end( "{"+ip+", "+lang+", "+client_info+"}");
+
+});
+
+
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
